d4061025e420d5dee235aa0af33988b0a30f329e
Boyan Iliev <97385003+Boyan253@users.noreply.github.com>
2023-04-17 16:04:38 +0300

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,13 +1,13 @@
 const config = {
   production: {
     PORT: 2222,
-    DB_URI: "mongodb://127.0.0.1:27017/hotel",
+    DB_URI: `mongodb+srv://bobosa2534:Su18hK5FCW2llkkK@cluster0.lh5vqlc.mongodb.net/?retryWrites=true&w=majority`,
     SECRET: "F.O.U.R.secret",
     ONE_DAY: 1000 * 60 * 60 * 24,
   },
   development: {
     PORT: 3000,
-    DB_URI: "mongodb://127.0.0.1:27017/hotelApp",
+    DB_URI: `mongodb+srv://bobosa2534:Su18hK5FCW2llkkK@cluster0.lh5vqlc.mongodb.net/?retryWrites=true&w=majority`,
     SECRET: "F.O.U.R.secret",
     ONE_DAY: 1000 * 60 * 60 * 24,
   },
